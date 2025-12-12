499112072520a0dd5a39629266ff154712218a85
Nikhil Chodavarapu <nikhilchodavarapu@Nikhils-MacBook-Pro.local>
2025-12-12 17:50:36 +0530

    Day14

--- advent-of-code-2019/day14-space-stoichiometry/min-amount-ore.js
+++ advent-of-code-2019/day14-space-stoichiometry/min-amount-ore.js
@@ -5,38 +5,55 @@
 // 7 A, 1 D => 1 E
 // 7 A, 1 E => 1 FUEL
 
-const convert = (reactions, [value, key], quantities) => {
-  let quantityOfOre = 0;
-  console.log(value, key, "\n\n");
-  if (reactions[key] !== undefined) {
-    for (const element of reactions[key]) {
-      const material = element.split(" ");
+const isEveryThingORE = (reactions, material) =>
+  reactions[material].every((x) => x.split(" ")[1] === "ORE");
+
+const convert = (reactions, key, quantities) => {
+  for (const element of reactions[key]) {
+    const material = element.split(" ");
+    console.log(reactions);
+    if (
+      reactions[material[1]] !== undefined &&
+      !(reactions[material[1]].length === 1 &&
+        reactions[material[1]][0].split(" ")[1] === "ORE")
+    ) {
+      const quantity = quantities[material[1]];
       const materialQuantity = parseInt(material[0]);
-      if (material[1] === "ORE") {
-        quantityOfOre += materialQuantity;
-        console.log(quantityOfOre);
-      } else {
-        const quantity = quantities[material[1]];
-        let requiredQuantity = materialQuantity / quantity;
-        if (materialQuantity % quantity !== 0) {
-          const mayReqQuantity =
-            (materialQuantity + (quantity - materialQuantity % quantity)) /
-            quantity;
-          requiredQuantity = mayReqQuantity === 1 ? quantity : mayReqQuantity;
-        }
-        console.log(quantity, materialQuantity, requiredQuantity);
-        quantityOfOre += requiredQuantity *
-          convert(reactions, material, quantities);
-        console.log(quantityOfOre);
+      let requiredQuantity = materialQuantity / quantity;
+      if (materialQuantity % quantity !== 0) {
+        requiredQuantity =
+          (materialQuantity + (quantity - materialQuantity % quantity)) /
+          quantity;
       }
+      reactions[material[1]] = reactions[material[1]].map((x) => {
+        const element = x.split(" ");
+        const quantity = requiredQuantity * parseInt(element[0]);
+        return `${quantity} ${element[1]}`;
+      });
     }
   }
-  return quantityOfOre;
+  reactions[key] = reactions[key].flatMap((x) => {
+    const element = x.split(" ");
+    if (element[1] === "ORE") {
+      return x;
+    }
+    return reactions[element[1]];
+  });
+  if (!isEveryThingORE(reactions, key)) convert(reactions, key, quantities);
+  reactions[key] = reactions[key].reduce((total, x) => {
+    const element = x.split(" ");
+    console.log(total);
+    total[element[1]] = total[element[1]] || 0;
+    total[element[1]] += +element[0];
+    return total;
+  }, {});
+  reactions[key] = Object.entries(reactions[key]).map(([k, v]) => `${v} ${k}`);
+  return reactions;
 };
 
 const findMinAmountOfORE = () => {
   const input = Deno.readTextFileSync("input.txt");
-  const reactions = input.split("\r\n").map((x) => x.split(" => ")).reduce(
+  const reactions = input.split("\n").map((x) => x.split(" => ")).reduce(
     (reactions, x) => (reactions[x[1]] = x[0].split(", ")) && reactions,
     {},
   );
@@ -58,7 +75,7 @@ const findMinAmountOfORE = () => {
     (obj, [key, val]) => (obj[key.split(" ")[1]] = val) && obj,
     {},
   );
-  console.log(updatedReactions);
+  // console.log(updatedReactions);
 
   // let quantityOfOre = 0;
   // for (const element of updatedReactions.FUEL) {
@@ -75,7 +92,7 @@ const findMinAmountOfORE = () => {
   //   console.log("\n\n");
   // }
 
-  console.log(convert(updatedReactions, [1, "FUEL"], quantities));
+  console.log(convert(updatedReactions, "FUEL", quantities));
   // for (const key in reactions) {
   //   reactions[key] = reactions[key].map((x) => {
   //     const splitted = x.split(" ");
@@ -85,4 +102,4 @@ const findMinAmountOfORE = () => {
   return reactions;
 };
 
-console.log(findMinAmountOfORE());
+findMinAmountOfORE();
