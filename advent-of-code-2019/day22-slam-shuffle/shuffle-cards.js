c7ff6c2366d0ac5442e376b4b30f7d7340f3acde
Your Name <you@example.com>
2025-12-21 01:31:39 +0530

    Day22 - make something that will give position of any req number after shufffling (But that's not the problem to solve)

--- advent-of-code-2019/day22-slam-shuffle/shuffle-cards.js
+++ advent-of-code-2019/day22-slam-shuffle/shuffle-cards.js
@@ -5,13 +5,23 @@ const dealStack = (cards, increment = 0) => {
   for (let i = 0; i < cards.length; i++) {
     newStack[j] = cards[i];
     j += reqIncr;
-    j = j % cards.length || 1;
+    j = j % cards.length;
   }
   if (!increment) {
     newStack.push(newStack.shift());
   }
-  console.log(newStack);
   return newStack;
+};
+
+const dealStackIndex = (length, index, increment) => {
+  if (index === 1 && increment) return index;
+  if (!increment) return length - index - 1;
+  return (index * increment) % length;
+};
+
+const cutStackIndex = (length, index, noOfCards) => {
+  if (noOfCards > 0) return (length - noOfCards + index) % length;
+  else return (index - noOfCards) % length;
 };
 
 const cut = (cards, noOfCards) => {
@@ -28,23 +38,33 @@ const parseInput = () => {
   const input = Deno.readTextFileSync("input.txt").split("\n");
   return input.map((x) => {
     const reqFn = x.split(" ");
-    if (reqFn[0] === "cut") return [cut, +reqFn[1]];
-    else return [dealStack, +reqFn[3]];
+    // if (reqFn[0] === "cut") return [cut, +reqFn[1]];
+    // else return [dealStack, +reqFn[3]];
+    if (reqFn[0] === "cut") return [cutStackIndex, +reqFn[1]];
+    else return [dealStackIndex, +reqFn[3]];
   });
 };
 
 const shuffleCards = () => {
-  const cards = [];
-  for (let i = 0; i < 10007; i++) {
-    cards[i] = i;
-  }
+  // const cards = [];
+  // for (let i = 0; i < 10007; i++) {
+  //   cards[i] = i;
+  // }
   const shuffleProcess = parseInput();
-  let shuffledStack = [...cards];
+  // let shuffledStack = [...cards];
+  // shuffleProcess.forEach((process) => {
+  //   shuffledStack = process[0](shuffledStack, process[1]);\
+  // });
+  // console.log(shuffledStack.indexOf(2019));
+  // return shuffledStack.indexOf(2019);
+
+  let index = 2020;
+  const length = 101741582076661;
   shuffleProcess.forEach((process) => {
-    shuffledStack = process[0](shuffledStack, process[1]);
+    index = process[0](length, index, process[1]);
   });
-  console.log(shuffledStack.indexOf(2019));
-  return shuffledStack.indexOf(2019);
+  console.log(index);
+  return index;
 };
 
 shuffleCards();
