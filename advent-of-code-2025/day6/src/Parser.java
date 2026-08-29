26de3ccf141ffb004b4c8c95527daea861d1c03a
Nikhil <iamnikhilchodavarapu@gmail.com>
2026-08-29 15:10:56 +0530

    2025-day-6 part two successfully completed

--- advent-of-code-2025/day6/src/Parser.java
+++ advent-of-code-2025/day6/src/Parser.java
@@ -5,7 +5,8 @@ public class Parser {
     public static Data parse(String input) {
         String[] numbersWithOperators = input.split("\n");
         String[] operators = numbersWithOperators[numbersWithOperators.length - 1].trim().split(" +");
-        String[] numbers = Arrays.copyOfRange(numbersWithOperators, 0, numbersWithOperators.length - 1);
+        String[] numbers = Arrays.stream(Arrays.copyOfRange(numbersWithOperators, 0, numbersWithOperators.length - 1))
+                .map(x -> x).toArray(String[]::new);
         return new Data(numbers, operators);
     }
 }
